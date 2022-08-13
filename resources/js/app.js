077c2488bd52005b5a3a4b0620b6e84478b0ec2e
Angelo Arcillas <angeloarcillas64@gmail.com>
2022-08-13 14:15:05 +0800

    Add vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
-import './bootstrap';
+import "./bootstrap";
+import "../css/app.css";
+
+import { createApp } from "vue/dist/vue.esm-bundler.js";
+
+const app = createApp({
+    data() {
+        return {
+            message: "Hello Vue!",
+        };
+    },
+});
+
+app.mount("#app");
